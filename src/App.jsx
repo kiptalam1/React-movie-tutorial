4cc75f8c5fd18e3980d374e59e50fd007afa6ca1
adams <adamskiptalam0@gmail.com>
2025-03-20 20:24:34 +0300

    Add the Home Page component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
+
 
 function App() {
-	const movie = {
-		url: "#",
-		title: "Adams family",
-		release_date: "2010",
-	};
+	
 	return (
 		<>
 			<h1>movies</h1>
-			<MovieCard movie={movie} />
-			<MovieCard movie={movie} />
+			<Home />
 		</>
 	);
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,56 @@
+import MovieCard from "../components/MovieCard";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+export default function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+	const movies = [
+		{
+			id: 1,
+			url: "src/assets/download.jpeg",
+			title: "The Adams Family",
+			release_date: "2019",
+		},
+		{
+			id: 2,
+			url: "src/assets/download.jpeg",
+			title: "The Adams Family",
+			release_date: "2019",
+		},
+		{
+			id: 3,
+			url: "src/assets/download.jpeg",
+			title: "The Adams Family",
+			release_date: "2019",
+		},
+	];
+
+	const handleSearch = (e) => {
+		e.preventDefault();
+		alert(searchQuery);
+        setSearchQuery(""); // clear the search input
+	};
+	return (
+		<div className='home'>
+			<form className={styles.searchForm} onSubmit={handleSearch}>
+				<input
+					className={styles.searchInput}
+					type='text'
+					placeholder='search movies...'
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+				/>
+				<button
+					type='submit'
+					className={styles.searchBtn}
+					onClick={handleSearch}
+				>
+					Search
+				</button>
+			</form>
+
+			{movies.map((movie) => (
+				<MovieCard movie={movie} key={movie.id} />
+			))}
+		</div>
+	);
+}
